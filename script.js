9ded787d086735d4c0d645dcbc02dd226330557a
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-10 05:21:20 +0200

    group repeated codes in a function, called function on buttons

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function playGame(playerMove) {
+    const computerMove = getComputerChoice();
+
+    let result;
+
+    if (playerMove === 'Scissors') {
+
+        if (computerMove === 'Rock') {
+            result = 'You Lose!';
+        } else if (computerMove === 'Paper') {
+            result = 'You Win!';
+        } else if (computerMove === 'Scissors') {
+            result = 'Tie';
+        }
+
+    } else if (playerMove === 'Paper') {
+
+         if (computerMove === 'Rock') {
+            result = 'You Win!';
+        } else if (computerMove === 'Paper') {
+            result = 'Tie';
+        } else if (computerMove === 'Scissors') {
+            result = 'You Lose!';
+        }
+
+    } else if (playerMove === 'Rock') {
+
+        if (computerMove === 'Rock') {
+            result = 'Tie';
+        } else if (computerMove === 'Paper') {
+            result = 'You Lose!';
+        } else if (computerMove === 'Scissors') {
+            result = 'You Win!';
+        }
+    }
+        
+
+    console.log(result);
+
+    alert(`You picked ${playerMove}, Computer picked ${computerMove}. ${result}`);
+}
 
 
 function getComputerChoice () {
